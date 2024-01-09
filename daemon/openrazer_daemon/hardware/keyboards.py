991e2b6fe1a611572f70df14ecef265b4cd0bdce
Khraut <82505643+Khraut@users.noreply.github.com>
2024-01-09 12:53:51 +0100

    Update keyboards.py
    Changed the matrix size

--- daemon/openrazer_daemon/hardware/keyboards.py
+++ daemon/openrazer_daemon/hardware/keyboards.py
@@ -1447,7 +1447,7 @@ class RazerBook2020(_MacroKeyboard):
     USB_VID = 0x1532
     USB_PID = 0x026A
     HAS_MATRIX = True
-    MATRIX_DIMS = [6, 16]
+    MATRIX_DIMS = [6, 15]
     METHODS = ['get_device_type_keyboard', 'set_wave_effect', 'set_static_effect', 'set_spectrum_effect',
                'set_reactive_effect', 'set_none_effect', 'set_breath_random_effect', 'set_breath_single_effect',
                'set_breath_dual_effect', 'set_custom_effect', 'set_key_row',
